c0cb90aff10dc509bd1e335d88f174f740f827d2
Elijah Irizarry <user1@localhost.localdomain>
2017-12-07 23:05:55 -0600

    Changed the name of variable

--- js/site.js
+++ js/site.js
@@ -4,17 +4,17 @@ $.noConflict();
 $('#form').on('submit', function(e){
     var username = $('#nameinput').val();
     var useremail = $('#emailinput').val();
-    var userpsn = $('#textinput').val();
+    var userid = $('#textinput').val();
     var Confirmation = document.getElementById("confirmation");
     var fill = "Please fill in all the fields displayed above";
-    var troll = "HAH JK, I PLAY on Steam Actually. PC for the win always!";
+    var troll = "HAH JK, I PLAY on PS4 Actually. PS for the win always!";
     e.preventDefault();
-    if (username === '' || useremail === '' || userpsn === '') {
+    if (username === '' || useremail === '' || userid === '') {
       confirmation.textContent = fill;
       document.getElementById("confirmation").style.color = 'black';
 
     } else{
-      console.log("Submission Completed", username, useremail, userpsn);
+      console.log("Submission Completed", username, useremail, userid);
       confirmation.textContent = troll;
       document.getElementById("confirmation").style.color = 'skyblue';
     }
