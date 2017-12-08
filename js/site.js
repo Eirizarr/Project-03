89bf3e245f6f22c1b45e9df6452049eb49adc353
Elijah Irizarry <user1@localhost.localdomain>
2017-12-07 21:49:04 -0600

    Had to revise a variable

--- js/site.js
+++ js/site.js
@@ -1,18 +1,18 @@
 $('#form').on('submit', function(e){
     var username = $('#nameinput').val();
     var useremail = $('#emailinput').val();
-    var userquestion = $('#textinput').val();
+    var userpsn = $('#textinput').val();
     var Confirmation = document.getElementById("confirmation");
     var fill = "Please fill in all the fields displayed above";
     var troll = "HAH JK, I PLAY on Steam Actually. PC for the win always!";
     e.preventDefault();
     if (name === '' || email === '' || question === '') {
-      status.textContent = fill;
+      confirmation.textContent = fill;
       document.getElementById("confirmation").style.color = 'black';
 
     } else{
       console.log("Submission Completed", name, email, question);
-      status.textContent = troll;
+      confirmation.textContent = troll;
       document.getElementById("confirmation").style.color = 'skyblue';
     }
 
